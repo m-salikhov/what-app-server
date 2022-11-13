33a5545d06e219e62a02f386ce37b8c598b8a6c8
m-salikhov <andvary@inbox.ru>
2022-11-13 20:25:24 +0300

    c6

--- dist/Modules/auth/auth.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthService } from './auth.service';
-import { Response } from 'express';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    login(req: any, response: Response): Promise<any>;
-    loginFirst(req: any, response: Response): Promise<import("../users/entity/user.entity").User>;
-    logout(response: Response, req: any): string;
-}
--- dist/Modules/tournaments/entities/sourse.entity.d.ts
+++
@@ -1,6 +0,0 @@
-import { Question } from './question.entity';
-export declare class Source {
-    id: number;
-    link: string;
-    question: Question;
-}

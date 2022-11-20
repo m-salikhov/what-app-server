34b4fa31157a2abc0b0619dae2043f2c8eb9f761
m-salikhov <andvary@inbox.ru>
2022-11-20 20:27:22 +0300

    c14 cors

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -23,6 +23,8 @@ export class AuthController {
     response.cookie('access_token', token.access_token, {
       httpOnly: true,
       maxAge: 86_400_000,
+      sameSite: 'none',
+      secure: true,
     });
     return req.user;
   }

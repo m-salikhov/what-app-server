a291f8d64223de5f4ee6cbf545d5e149d2cc724b
m-salikhov <andvary@inbox.ru>
2022-11-27 20:39:25 +0300

    c19

--- src/main.ts
+++ src/main.ts
@@ -4,31 +4,30 @@ import * as cookieParser from 'cookie-parser';
 import * as fs from 'fs';
 import { join } from 'path';
 
-// const httpsOptions = {
-//   // key: fs.readFileSync(join(process.cwd(), './src/secret/key.pem')).toString(),
-//   key: fs.readFileSync('src/secret/key.pem'),
-//   cert: fs
-//     .readFileSync(join(process.cwd(), './src/secret/cert.pem'))
-//     .toString(),
-// };
+const httpsOptions = {
+  // key: fs.readFileSync(join(process.cwd(), './src/secret/key.pem')).toString(),
+  key: fs.readFileSync('src/secret/key.pem'),
+  cert: fs
+    .readFileSync(join(process.cwd(), './src/secret/cert.pem'))
+    .toString(),
+};
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule, { httpsOptions });
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   app.use(cookieParser());
-  // app.enableCors({
-  //   origin: [
-  //     'https://localhost:3000',
-  //     'https://62.217.179.200:3000',
-  //     'http://62.217.179.200:3000',
-  //     'https://62.217.179.200',
-  //     'http://62.217.179.200',
-  //     'https://sage-paletas-ab37e6.netlify.app',
-  //     'http://sage-paletas-ab37e6.netlify.app',
-  //   ],
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: [
+      'https://localhost:3000',
+      'https://62.217.179.200:3000',
+      'http://62.217.179.200:3000',
+      'https://62.217.179.200',
+      'http://62.217.179.200',
+      'https://sage-paletas-ab37e6.netlify.app',
+      'http://sage-paletas-ab37e6.netlify.app',
+    ],
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();

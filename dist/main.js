23b6f2631ef45ca7867942145b87daabd5442430
m-salikhov <andvary@inbox.ru>
2022-12-04 00:13:32 +0300

    c19

--- dist/main.js
+++ dist/main.js
@@ -3,29 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const cookieParser = require("cookie-parser");
-const fs = require("fs");
-const path_1 = require("path");
-const httpsOptions = {
-    key: fs.readFileSync('src/secret/key.pem'),
-    cert: fs
-        .readFileSync((0, path_1.join)(process.cwd(), './src/secret/cert.pem'))
-        .toString(),
-};
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { httpsOptions });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.use(cookieParser());
-    app.enableCors({
-        origin: [
-            'https://localhost:3000',
-            'https://62.217.179.200:3000',
-            'http://62.217.179.200:3000',
-            'https://62.217.179.200',
-            'http://62.217.179.200',
-            'https://sage-paletas-ab37e6.netlify.app',
-            'http://sage-paletas-ab37e6.netlify.app',
-        ],
-        credentials: true,
-    });
     await app.listen(process.env.PORT || 5000);
 }
 bootstrap();

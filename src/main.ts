fcbd269022d285a8ef9050156ed175610be89605
m-salikhov <andvary@inbox.ru>
2022-12-04 00:13:32 +0300

    c19

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import * as fs from 'fs';
-import { join } from 'path';
+// import * as fs from 'fs';
+// import { join } from 'path';
 
-const httpsOptions = {
-  // key: fs.readFileSync(join(process.cwd(), './src/secret/key.pem')).toString(),
-  key: fs.readFileSync('src/secret/key.pem'),
-  cert: fs
-    .readFileSync(join(process.cwd(), './src/secret/cert.pem'))
-    .toString(),
-};
+// const httpsOptions = {
+//   // key: fs.readFileSync(join(process.cwd(), './src/secret/key.pem')).toString(),
+//   key: fs.readFileSync('src/secret/key.pem'),
+//   cert: fs
+//     .readFileSync(join(process.cwd(), './src/secret/cert.pem'))
+//     .toString(),
+// };
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
-  app.enableCors({
-    origin: [
-      'https://localhost:3000',
-      'https://62.217.179.200:3000',
-      'http://62.217.179.200:3000',
-      'https://62.217.179.200',
-      'http://62.217.179.200',
-      'https://4gk-base.netlify.app',
-      'http://4gk-base.netlify.app',
-    ],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'https://localhost:3000',
+  //     'https://62.217.179.200:3000',
+  //     'http://62.217.179.200:3000',
+  //     'https://62.217.179.200',
+  //     'http://62.217.179.200',
+  //     'https://4gk-base.netlify.app',
+  //     'http://4gk-base.netlify.app',
+  //   ],
+  //   credentials: true,
+  // });
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();

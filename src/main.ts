5e92af8a60fb07ad8ca6024bda33d993328f8fdf
m-salikhov <andvary@inbox.ru>
2023-08-12 16:57:57 +0300

    fix request size

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { json } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.use(json({ limit: '50mb' }));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }

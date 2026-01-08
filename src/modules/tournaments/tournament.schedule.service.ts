02e58511c94e2503ef8f2906423dcd042545f611
m-salikhov <andvary@inbox.ru>
2026-01-08 20:15:31 +0300

    feat: проверка использован ли пример для парсинга

--- src/modules/tournaments/tournament.schedule.service.ts
+++ src/modules/tournaments/tournament.schedule.service.ts
@@ -4,10 +4,18 @@ import { MailService } from "../mail/mail.service";
 import { parseTournamentGotquestions } from "./helpers/parse-link.helper";
 import { diff } from "just-diff";
 import target from "./fixtures/tournament-target.json";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Tournament } from "./entities/tournament.entity";
 
 @Injectable()
 export class TournamentScheduleService {
-	constructor(private readonly mailService: MailService) {}
+	constructor(
+		private readonly mailService: MailService,
+
+		@InjectRepository(Tournament)
+		private tournamentRepo: Repository<Tournament>,
+	) {}
 
 	@Cron(CronExpression.EVERY_DAY_AT_3AM)
 	async handleCron() {
@@ -16,11 +24,11 @@ export class TournamentScheduleService {
 			date: new Date(target.date),
 		};
 
+		const linkTarget = "https://gotquestions.online/pack/394";
+
 		try {
 			// убираем dateUpload - это всегда new Date()
-			const { dateUpload, ...parsedTournament } = await parseTournamentGotquestions(
-				"https://gotquestions.online/pack/394",
-			);
+			const { dateUpload, ...parsedTournament } = await parseTournamentGotquestions(linkTarget);
 
 			const res = diff(targetTournament, parsedTournament);
 
@@ -31,6 +39,21 @@ export class TournamentScheduleService {
 			}
 		} catch (error) {
 			this.mailService.sendAdminEmail("Parse test error", error.toString());
+		}
+	}
+
+	// проверка использован ли пример
+	// на странице https://4gk-base.andvarif.ru/addbylink
+	@Cron(CronExpression.EVERY_6_HOURS)
+	async checkSample() {
+		const link = "https://gotquestions.online/pack/6001";
+
+		const tournamentCheck = await this.tournamentRepo.findOne({
+			where: { link },
+		});
+
+		if (tournamentCheck) {
+			this.mailService.sendAdminEmail("Пример использован", `турнир ${link} был добавлен в базу`);
 		}
 	}
 }
